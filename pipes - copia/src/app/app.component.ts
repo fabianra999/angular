a401d6cfde7558cfec8c8519d9f7bc79da41763e
desbog7 <desbog7@DESBOG7>
2018-03-20 09:38:41 -0500

    pipis test

---
+++ pipes - copia/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import { Promise } from 'q';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre: string = 'Tomas';
+
+  arreglo: any[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI: number = Math.PI;
+
+  a: number = 0.234;
+
+  salario: number = 1234.5;
+
+  heroe = {
+    nombre: "Logan",
+    clave: "Wolverin",
+    edad: "500",
+    direccion: {
+      caller: "Primera",
+      casa: "19"
+    }
+  };
+
+ /*  valorDePromesa = new Promise ( ( resolve, reject ) => {
+    setTimeout( () => resolve ('llego la data'),350 );
+  }); */
+
+}
